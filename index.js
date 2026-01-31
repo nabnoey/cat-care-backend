444614979f0b77728db829f4a2367ef977a7f240
nabnoey <nabnoeyy@gmail.com>
2026-02-01 01:19:53 +0700

    edit index

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-   origin: [process.env.BASE_URL, "https://cat-care-frontend.vercel.app", "http://localhost:5173"],
+   origin: [process.env.BASE_URL, "https://cat-care-frontend.vercel.app/api/v1", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE"],
    allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
