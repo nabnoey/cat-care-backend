cf3000a5f51f43a100e3503c18bb540dde943192
nabnoey <nabnoeyy@gmail.com>
2026-01-31 23:40:19 +0700

    edit cors3

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-   origin: "http://localhost:5173",
+   origin: process.env.BASE_URL,
   methods: ["GET", "POST", "PUT", "DELETE"],
    allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
